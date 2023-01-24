327063ef2a9ad34b63c5cc0f35e72e5e5ffb89d0
Maks S <Gmagz@yandex.ru>
2023-01-24 23:06:47 +0300

    Rework-2

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,23 @@ public class Main {
     public static void main(String[] args) {
 
         int checkClient = 100;
-        int refill = 1200;
+        int refill = 1000;
 
-        int percent = 1;
+        int percent;
         if (refill >= 1000) {
-            }
+            percent = 1;
+            int bonus = refill * percent / 100;
+            int totalClientAmount = checkClient + bonus;
 
-        int bonus = refill * percent / 100;
-        int totalClientAmount = checkClient + refill * percent / 100;
+            System.out.println(" Итого начисленно бонусных рублей: " + bonus);
+            System.out.println(" Итого на счету клиента: " + totalClientAmount);
+        } else {
+            int bonus = 0;
+            int totalClientAmount = checkClient + bonus;
 
-        System.out.println(" Итого начисленно бонусных рублей: " + bonus);
-        System.out.println(" Итого на счету клиента: " + totalClientAmount);
-
+            System.out.println(" Итого начисленно бонусных рублей: " + bonus);
+            System.out.println(" Итого на счету клиента: " + totalClientAmount);
+        }
 
     }
 }
